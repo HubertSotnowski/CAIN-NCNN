5dc40e250f2e13c38125678eb396a7d0e65288af
Hubert Sontowski <72866544+HubertSotnowski@users.noreply.github.com>
2022-11-13 00:22:38 +0100

    Updated convert script

--- convert.py
+++ convert.py
@@ -1,8 +1,13 @@
 from cain.cain import CAIN
 import torch
+import argparse
+my_parser = argparse.ArgumentParser(description=" ")
+my_parser.add_argument("--input", metavar="--input", type=str, help="input model")
+args = my_parser.parse_args()
+
 import os
 model=CAIN(3)
-model.load_state_dict(torch.load('1.pth'),strict=False)
+model.load_state_dict(torch.load(args.input), strict=False)
 input_names = ["in1", "in2"]
 output_names = ["output_frame"]
 f1=torch.rand((1,3,256,256))
@@ -17,3 +22,5 @@ torch.onnx.export(model,               # model being run
                   input_names = input_names,   # the model's input names
                   output_names = output_names) # the model's output names)
 os.system("python3 -m onnxsim cain.onnx cain-sim.onnx")
+
+
